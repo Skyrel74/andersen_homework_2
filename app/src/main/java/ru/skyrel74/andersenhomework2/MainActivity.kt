e93ab602e0165ea7b56a67223b1774721d9743ea
Ilya Rakipov <ilya.rakipov@gmail.com>
2021-09-29 18:58:01 +0500

    Initial commit

---
+++ app/src/main/java/ru/skyrel74/andersenhomework2/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.skyrel74.andersenhomework2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
