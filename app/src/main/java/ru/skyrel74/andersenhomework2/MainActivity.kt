1483f476be2a9725f3d94fb4021e0f663519fb75
Ilya Rakipov <ilya.rakipov@gmail.com>
2021-09-30 01:43:32 +0500

    1.2 Part A: Your first interactive UI
    Signed-off-by: Ilya Rakipov <ilya.rakipov@gmail.com>

--- app/src/main/java/ru/skyrel74/andersenhomework2/MainActivity.kt
+++ app/src/main/java/ru/skyrel74/andersenhomework2/MainActivity.kt
@@ -2,17 +2,30 @@ package ru.skyrel74.andersenhomework2
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private val LOG_TAG = MainActivity::class.java.simpleName
 
+    private var mCount = 0
+    private lateinit var mShowCount: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mShowCount = findViewById(R.id.show_count)
 
         runCatching { throw Exception("Some message for logging") }
             .onFailure { error -> Log.e(LOG_TAG, error.message) }
+    }
+
+    fun showToast(view: View): Unit =
+        Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT).show()
+
+    fun countUp(view: View): Unit {
+        mShowCount.text = (++mCount).toString()
     }
 }
