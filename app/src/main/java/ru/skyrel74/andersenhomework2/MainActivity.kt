4650b05ccefd7d87155a4de8f7f94a4276f458e9
Ilya Rakipov <ilya.rakipov@gmail.com>
2021-09-29 23:18:26 +0500

    1.1: Android Studio and Hello World
    Signed-off-by: Ilya Rakipov <ilya.rakipov@gmail.com>

--- app/src/main/java/ru/skyrel74/andersenhomework2/MainActivity.kt
+++ app/src/main/java/ru/skyrel74/andersenhomework2/MainActivity.kt
@@ -1,12 +1,18 @@
 package ru.skyrel74.andersenhomework2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val LOG_TAG = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        runCatching { throw Exception("Some message for logging") }
+            .onFailure { error -> Log.e(LOG_TAG, error.message) }
     }
 }
