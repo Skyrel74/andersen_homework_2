881586bdd0f37e404b05c9ba2d3b3898dd361314
Ilya Rakipov <ilya.rakipov@gmail.com>
2021-09-30 23:48:24 +0500

    1.2 Part B: The layout editor
    Signed-off-by: Ilya Rakipov <ilya.rakipov@gmail.com>

--- app/src/main/java/ru/skyrel74/andersenhomework2/MainActivity.kt
+++ app/src/main/java/ru/skyrel74/andersenhomework2/MainActivity.kt
@@ -1,8 +1,10 @@
 package ru.skyrel74.andersenhomework2
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -13,10 +15,12 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private var mCount = 0
     private lateinit var mShowCount: TextView
+    private lateinit var mButtonZero: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mShowCount = findViewById(R.id.show_count)
+        mButtonZero = findViewById(R.id.button_zero)
 
         runCatching { throw Exception("Some message for logging") }
             .onFailure { error -> Log.e(LOG_TAG, error.message) }
@@ -27,5 +31,21 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     fun countUp(view: View): Unit {
         mShowCount.text = (++mCount).toString()
+
+        val color = if (mCount % 2 == 0) {
+            Color.RED
+        } else {
+            Color.GREEN
+        }
+        view.setBackgroundColor(color)
+
+        if (mCount > 0) {
+            mButtonZero.setBackgroundColor(Color.CYAN)
+        }
+    }
+
+    fun zeroize(view: View): Unit {
+        mCount = 0
+        mShowCount.text = mCount.toString()
     }
 }
